e1cc4b3d1475afe6e9e435359dc380187c528701
Joelios <joel.mesmer@libracore.com>
2020-10-30 10:29:13 +0100

    TASK00437

---
+++ senstech/senstech/doctype/senstech_funktion/senstech_funktion.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2020, libracore AG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Senstech Funktion', {
+	// refresh: function(frm) {
+
+	// }
+});
