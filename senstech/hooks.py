c2ec3aed57a61a68b2c135c6d2ab5d9cb97ed9f7
Joelios <joel.mesmer@libracore.com>
2020-12-17 16:03:40 +0100

    TASK00454

--- senstech/hooks.py
+++ senstech/hooks.py
@@ -15,7 +15,7 @@ app_license = "MIT"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/senstech/css/senstech.css"
+app_include_css = "/assets/senstech/css/senstech.css"
 app_include_js = "/assets/senstech/js/senstech.js"
 
 # include js, css files in header of web template
