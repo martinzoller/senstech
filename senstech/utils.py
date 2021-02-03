f7040cb3c6bda98194d87dc95314c97fe36dcc8b
Joelios <joel.mesmer@libracore.com>
2021-02-03 09:47:02 +0100

    TASK00508

--- senstech/utils.py
+++ senstech/utils.py
@@ -211,3 +211,34 @@ def calculate_versanddatum(so):
             return 'updated'
         except:
             return 'passed'
+            
+@frappe.whitelist()
+def create_payment(sinv):
+    try:
+        sinv = frappe.get_doc("Sales Invoice", sinv)
+        pe = frappe.get_doc({
+            "doctype": "Payment Entry",
+            'posting_date': today(),
+            "payment_type": "Receive",
+            "party_type": "Customer",
+            "party": sinv.customer,
+            "paid_from": '1100 - Forderungen Schweiz - ST',
+            'paid_to': '1020 - KK ZKB 1042-0171.171 - ST',
+            'paid_amount': sinv.outstanding_amount,
+            'received_amount': sinv.outstanding_amount,
+            "references": [
+                {
+                    "reference_doctype": "Sales Invoice",
+                    'reference_name': sinv.name,
+                    'allocated_amount': sinv.outstanding_amount
+                }
+            ],
+            "reference_no": sinv.name,
+            'reference_date': today(),
+            'remarks': 'Auto Payment for {sinv}'.format(sinv=sinv.name)
+        })
+        pe.insert()
+        pe.submit()
+        return pe.name
+    except Exception as err:
+        return err
