52b0036c772d8630bd677ae3253276cd3879d77e
Martin Zoller <martin.zoller@senstech.ch>
2021-12-22 19:22:01 +0100

    Blanket Order 'To' Date: Include tick 'Until further notice'

--- senstech/utils.py
+++ senstech/utils.py
@@ -566,6 +566,8 @@ def direct_print_pdf(file):
     return
     
 @frappe.whitelist()
-def change_blanket_order_to_date(bo, date):
-    frappe.db.sql("""UPDATE `tabBlanket Order` SET `to_date` = '{date}' WHERE `name` = '{bo}'""".format(date=date, bo=bo), as_list=True)
+def change_blanket_order_to_date(bo, date, bis_auf_weiteres):
+    if bis_auf_weiteres == "1":
+        date = '2099-12-31'
+    frappe.db.sql("""UPDATE `tabBlanket Order` SET `to_date` = '{date}', `bis_auf_weiteres` = '{baw}' WHERE `name` = '{bo}'""".format(date=date, bo=bo, baw=bis_auf_weiteres), as_list=True)
     return
