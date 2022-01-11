96f67a1a50c9943baa7d3513fa483f31f66eda0b
Martin Zoller <martin.zoller@senstech.ch>
2022-01-11 09:28:34 +0100

    SO: Write-protect dates and improve Python function / Address: Improve search filter

--- senstech/scripts/sales_order_tools.py
+++ senstech/scripts/sales_order_tools.py
@@ -14,19 +14,15 @@ from frappe.utils.data import add_days
 def calculate_versanddatum(so):
     sales_order = frappe.get_doc("Sales Order", so)
     vorlauf = frappe.get_doc("Customer", sales_order.customer).vorlaufzeit_versand
-    if vorlauf and vorlauf > 0:
-        try:
-            new_date = add_days(sales_order.delivery_date, vorlauf * -1)
-            frappe.db.sql("""UPDATE `tabSales Order` SET `versanddatum` = '{new_date}' WHERE `name` = '{so}'""".format(new_date=new_date, so=so), as_list=True)
-            frappe.db.commit()
-            return 'updated'
-        except:
-            return 'passed'
-    else:
-        try:
-            new_date = sales_order.delivery_date
-            frappe.db.sql("""UPDATE `tabSales Order` SET `versanddatum` = '{new_date}' WHERE `name` = '{so}'""".format(new_date=new_date, so=so), as_list=True)
-            frappe.db.commit()
-            return 'updated'
-        except:
-            return 'passed'
+    if not vorlauf or vorlauf == 0:
+        if sales_order.territory == "Schweiz":
+            vorlauf = 1
+        else:
+            vorlauf = 7
+    try:
+        new_date = add_days(sales_order.delivery_date, vorlauf * -1)
+        frappe.db.sql("""UPDATE `tabSales Order` SET `versanddatum` = '{new_date}' WHERE `name` = '{so}'""".format(new_date=new_date, so=so), as_list=True)
+        frappe.db.commit()
+        return 'updated'
+    except:
+        return 'passed'
