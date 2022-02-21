6cd3670b29de7ef14158dcb88555ceb157faf411
Martin Zoller <martin.zoller@senstech.ch>
2022-02-21 17:09:17 +0100

    Add 'Go To' feature to Awesome Bar

--- senstech/hooks.py
+++ senstech/hooks.py
@@ -16,7 +16,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 app_include_css = "/assets/senstech/css/senstech.css"
-app_include_js = "/assets/senstech/js/senstech.js"
+app_include_js = [ "/assets/senstech/js/senstech.js", "/assets/senstech/js/awesome_bar_st.js" ]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/senstech/css/senstech.css"
