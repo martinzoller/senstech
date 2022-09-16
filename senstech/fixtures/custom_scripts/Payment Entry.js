02291d93eb4aa8d1709bf75ffcabd9810b153fda
lasalesi <larsericmueller@gmail.com>
2022-09-16 10:17:37 +0200

    Update Payment Entry.js
    adding deduction buttons

--- senstech/fixtures/custom_scripts/Payment Entry.js
+++ senstech/fixtures/custom_scripts/Payment Entry.js
@@ -2,5 +2,59 @@ frappe.ui.form.on('Payment Entry', {
 	onload_post_render(frm) {
         // Feld "Nummernkreis" lässt sich nicht mit Customization verstecken
         jQuery('div[data-fieldname="naming_series"]').hide();
+    },
+    refresh(frm) {
+        if (frm.doc.docstatus === 0) {
+            frm.add_custom_button(__("Abklärungskonto"), function() {
+                deduct_to_unclear(frm);
+            });
+            frm.add_custom_button(__("Kreditkarte"), function() {
+                credit_card(frm);
+            });
+            frm.add_custom_button(__("Rundung"), function() {
+                rounding(frm);
+            });
+            frm.add_custom_button(__("Spesen des Geldverkehrs"), function() {
+                bank_expenses(frm);
+            });
+            frm.add_custom_button(__("Löhne"), function() {
+                salaries(frm);
+            });
+            frm.add_custom_button(__("SVA"), function() {
+                sva(frm);
+            });
+        }
     }
-})
+});
+
+function deduct_to_unclear(frm) {
+    add_deduction("1097 - Durchlaufkonto Zahlungskonten - ST", "Haupt - ST", frm.doc.unallocated_amount);
+}
+
+function credit_card(frm) {
+    add_deduction("1095 - Firmenkreditkarte - ST", "Haupt - ST", cur_frm.doc.unallocated_amount);
+}
+
+function rounding(frm) {
+    add_deduction("6940 - Bank-/PC-Spesen - ST", "Haupt - ST", frm.doc.unallocated_amount);
+}
+
+function bank_expenses(frm) {
+    add_deduction("6940 - Bank-/PC-Spesen - ST", "Haupt - ST", frm.doc.unallocated_amount);
+}
+
+function salaries(frm) {
+    add_deduction("1091 - Lohndurchlaufkonto - ST", "Haupt - ST", cur_frm.doc.unallocated_amount);
+}
+
+function sva(frm) {
+    add_deduction("5700 - AHV / IV / EO - ST", "Haupt - ST", cur_frm.doc.unallocated_amount);
+}
+
+function add_deduction(account, cost_center, amount) {
+    var child = cur_frm.add_child('deductions');
+    frappe.model.set_value(child.doctype, child.name, 'account', account);
+    frappe.model.set_value(child.doctype, child.name, 'cost_center', cost_center);
+    frappe.model.set_value(child.doctype, child.name, 'amount', amount);
+    cur_frm.refresh_field('deductions');
+}
