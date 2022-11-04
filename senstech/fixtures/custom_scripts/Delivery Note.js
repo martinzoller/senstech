f68e361574ffec324080cca3fdc5a4ebe3120e86
Martin Zoller <martin.zoller@senstech.ch>
2022-11-04 11:27:26 +0100

    New feature: Assign sensor IDs to delivery note items, and automatically render a measurement report for these sensors

--- senstech/fixtures/custom_scripts/Delivery Note.js
+++ senstech/fixtures/custom_scripts/Delivery Note.js
@@ -52,13 +52,97 @@ frappe.ui.form.on('Delivery Note', {
     }
 })
 
-function reload_contacts(frm) {
-    var contact = cur_frm.doc.contact_person;
-    cur_frm.set_value("contact_person", "");
-    cur_frm.set_value("contact_person", contact);
-}
-
 frappe.ui.form.on('Delivery Note Item', {
+    item_code: function(frm) {
+		// Popup für Chargenauswahl ausblenden (erscheint ansonsten gefühlt genau dann nicht, wenn man es braucht, und umgekehrt...)
+        frappe.flags.hide_serial_batch_dialog = true;
+		// TODO: schauen, wie man die Hinweismeldung wegkriegt, dass es von keiner Charge genügend Stück hat - oder diese in ein unobtrusive popup notice verwandeln. Ebenso möglichst nicht automatisch die erstbeste Charge zuweisen!
+		// Evtl stattdessen einen Button machen, um Chargen zuzuweisen, oder dies sogar automatisch beim Laden eines neuen Dokuments tun - jeweils älteste mit Lagerbestand und so viele wie nötig, um die Gesamtzahl zu erreichen (automatisch Zeile duplizieren).
+		// Vielleicht kann man ein Override für die Funktion machen, welche aktuell die doofe Meldung anzeigt?!
+    },	
+    sensor_ids: function(frm, cdt, cdn) {
+		var current_item = locals[cdt][cdn];
+		var num_sensors_text = __('Anzahl Sensoren in der Liste:')+' '
+		
+		if(!current_item.item_code) {
+			frappe.msgprint(__("Bitte zuerst einen Artikel wählen"), __("Fehler"));
+		}
+        
+        let d = new frappe.ui.Dialog({
+			fields: [
+				{'fieldname': 'item_code', 'fieldtype': 'Link', 'options': 'Item', 'default': current_item.item_code, 'read_only': 1, 'label': __('Artikel')},
+				{'fieldname': 'batch_no', 'fieldtype': 'Link', 'options': 'Batch', 'default': current_item.batch_no, 'get_query': function() {
+					let filters = {
+						'item_code': current_item.item_code,
+						'posting_date': frm.doc.posting_date || frappe.datetime.nowdate(),
+					}
+					if (current_item.warehouse) filters["warehouse"] = current_item.warehouse;
+					return {
+						query : "erpnext.controllers.queries.get_batch_no",
+						filters: filters
+					}
+				}, 'reqd': 1, 'label': __('Produktionscharge'),'onchange': function() {
+					d.fields_dict.sensor_ids.set_value('');
+				}},
+				{'fieldname': 'qty', 'fieldtype': 'Int', 'default': current_item.qty, 'reqd': 1, 'label': __('Stückzahl')},
+				{'fieldname': 'sensor_ids', 'fieldtype': 'Text', 'reqd': 1, 'label': __('Sensor-IDs (kommagetrennt)'), 'onchange': function() {
+					var ids = d.fields_dict.sensor_ids.get_value().split(',');
+					d.fields_dict.sensor_ids.set_description(num_sensors_text+ids.length);
+				}},
+				{'fieldname': 'add_sensor_id', 'fieldtype': 'Small Text', 'reqd': 0, 'label': __('Sensor hinzufügen (Barcodescanner)')},
+			],
+			primary_action: function(values) {
+				var ids = values.sensor_ids.split(',');
+				if(ids.length != d.fields_dict.qty.get_value()) {
+					frappe.msgprint(__("Die Anzahl Sensor-IDs stimmt nicht mit der Stückzahl überein"),__("Fehler"));
+					return;
+				}
+				
+				var find_duplicates = arr => arr.filter((item, index) => arr.indexOf(item) !== index);
+				var duplicates = find_duplicates(ids);
+				if(duplicates.length > 0) {
+					frappe.msgprint(__("Die Liste der Sensor-IDs enthält Duplikate:")+" "+duplicates.join(','));
+					return;
+				}
+				
+				ids = ids.map(n => { return String(parseInt(n)).padStart(4,'0') });
+				
+				d.hide();
+				current_item.sensor_ids_list = ids.join(',');				
+				current_item.item_code = values.item_code;
+				current_item.batch_no = values.batch_no;
+				current_item.qty = values.qty;
+				frm.refresh_fields(['items']);
+			},
+			primary_action_label: __('Speichern')
+        });
+		d.fields_dict.add_sensor_id.wrapper.onkeypress = function(e) {
+			if(e.key=="Enter") {
+				e.preventDefault();
+				var cur_batch = d.fields_dict.batch_no.get_value();
+				var cur_item = d.fields_dict.item_code.get_value();
+				var barcode_value = d.fields_dict.add_sensor_id.get_value();
+				d.fields_dict.add_sensor_id.set_value('');				
+				if(!barcode_value) return;
+				var new_sensor_id = barcode_value.split("\n")[0];
+				if(new_sensor_id.startsWith(cur_batch+'/')){
+					var new_sensor_number = new_sensor_id.substr(cur_batch.length + 1);
+					var conditional_comma = [',',''].includes(d.fields_dict.sensor_ids.substr(-1)) ? '' : ',';
+					d.fields_dict.sensor_ids.set_value(d.fields_dict.sensor_ids.get_value()+conditional_comma+new_sensor_number);
+				}
+				else {
+					if(new_sensor_id.startsWith(cur_item)){
+						frappe.msgprint(__("Produktionscharge stimmt nicht mit der gewählten Charge überein."),__("Ungültige Sensor-ID"));
+					}
+					else {
+						frappe.msgprint(__("Artikel stimmt nicht mit dem gewählten Artikel überein."),__("Ungültige Sensor-ID"));
+					}
+				}
+			}
+		}
+		d.fields_dict.sensor_ids.set_value(current_item.sensor_ids_list); // Nicht als default, sondern im Nachhinein setzen, damit onchange triggert
+        d.show();
+    },
 	items_add: function(frm, cdt, cdn) {
       var current_item = locals[cdt][cdn];
       var all_items = cur_frm.doc.items;
@@ -88,6 +172,13 @@ frappe.ui.form.on('Delivery Note Item', {
       }
    }
 })
+
+
+function reload_contacts(frm) {
+    var contact = cur_frm.doc.contact_person;
+    cur_frm.set_value("contact_person", "");
+    cur_frm.set_value("contact_person", contact);
+}
 
 function fetch_taxes_and_charges_from_customer(frm) {
     if(!cur_frm.doc.customer) {
--- senstech/scripts/delivery_note_tools.py
+++ senstech/scripts/delivery_note_tools.py
@@ -52,3 +52,64 @@ def create_single_label_pdf(label_printer, item, batch, qty, output=None):
     
     label_html = frappe.render_template("senstech/templates/delivery_note_packing_unit_label.html", {"item": item, "batch": batch, "qty": qty})
     return frappe.utils.pdf.get_pdf(label_html, options, output)
+    
+
+def validate_sensor_ids(doc, method):
+    for item in doc.items:
+        if item.sensor_ids_list != '':
+            sensor_ids = item.sensor_ids_list.split(',')
+            if len(sensor_ids) != item.qty:
+                frappe.throw(_('Von Artikel "{item_name}" sollen {qty} Stück der Charge {chargennummer} geliefert werden, jedoch sind {id_count} Sensor-IDs erfasst').format(item_name=item.item_name,qty=item.qty,chargennummer=item.chargennummer,id_count=len(sensor_ids)))
+                return
+            for sensor_id in sensor_ids:
+                has_data = frappe.db.exists("Senstech Messdaten", {'batch': item.batch_no, 'sensor_id': sensor_id})
+                if not has_data:
+                    frappe.throw(_('Für Sensor Nr. {chargennummer}/{sensor_id} von Artikel "{item_name}" sind keine Messdaten verfügbar').format(chargennummer=item.chargennummer,sensor_id=sensor_id,item_name=item.item_name))
+                    return
+                existing_link = frappe.db.exists("Senstech Messdaten", {'batch': item.batch_no, 'sensor_id': sensor_id, 'included_in_delivery': ['!=','']})
+                if existing_link:
+                    dn_id = frappe.db.get_value("Senstech Messdaten", existing_link, "included_in_delivery")
+                    frappe.throw(_('Sensor Nr. {chargennummer}/{sensor_id} von Artikel "{item_name}" ist bereits in Lieferschein {dn_id} enthalten').format(chargennummer=item.chargennummer,sensor_id=sensor_id,item_name=item.item_name,dn_id=dn_id))
+                    return
+
+
+def assign_sensor_ids(doc, method):
+    for item in doc.items:
+        if item.sensor_ids_list != '':
+            sensor_ids = sanitize_sensor_ids(item.sensor_ids_list)
+            frappe.db.sql("""
+                UPDATE `tabSenstech Messdaten`
+                SET included_in_delivery='{dn_id}', delivered_to_customer='{cust_name}'
+                WHERE name IN
+                (
+                    SELECT md.name FROM
+                    (
+                        SELECT measurand,sensor_id,MAX(creation) AS creation FROM `tabSenstech Messdaten` WHERE sensor_id IN ({sensor_ids}) AND batch='{batch}' GROUP BY measurand,sensor_id
+                    ) AS latest_md
+                    INNER JOIN
+                    (
+                         -- This subquery is needed because of a MySQL quirk, see:
+                         -- https://stackoverflow.com/questions/44970574/table-is-specified-twice-both-as-a-target-for-update-and-as-a-separate-source
+                         SELECT * FROM `tabSenstech Messdaten`
+                    ) AS md
+                    ON latest_md.measurand = md.measurand AND latest_md.sensor_id = md.sensor_id AND latest_md.creation = md.creation
+                )                           
+            """.format(dn_id=doc.name, cust_name=doc.customer_name, sensor_ids=sensor_ids, batch=item.batch_no))
+            # The above query will write only the latest measurement record of each sensor and measurand.
+            # This is done by first selecting these records by MAX(creation), then finding their names by matching each field from the GROUP BY clause.
+            # The resulting list of names is then passed to the UPDATE query.
+
+def release_sensor_ids(doc, method):
+    frappe.db.sql("""
+        UPDATE `tabSenstech Messdaten`
+        SET included_in_delivery='', delivered_to_customer=''
+        WHERE included_in_delivery='{dn_id}'
+    """.format(dn_id=doc.name))            
+            
+        
+# Take a string list of sensor IDs, convert each ID to integer and back to string
+# (used as minimal data sanitization before SQL query)
+def sanitize_sensor_ids(sensor_ids):
+    sensor_ids_arr = sensor_ids.split(',')
+    sensor_ids_arr = list(map(lambda x: str(int(x)), sensor_ids_arr))
+    return ','.join(sensor_ids_arr)
