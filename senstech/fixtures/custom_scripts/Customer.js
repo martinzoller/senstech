5eeb1c2bcda361a60486004dbcf5ec2e969058d0
Martin Zoller <martin.zoller@senstech.ch>
2024-01-04 16:22:12 +0100

    Customer: New VAT rate, updated validations

--- senstech/fixtures/custom_scripts/Customer.js
+++ senstech/fixtures/custom_scripts/Customer.js
@@ -1,41 +1,46 @@
 frappe.ui.form.on('Customer', {
 	validate(frm) {
-	    if (cur_frm.doc.territory == 'Alle Regionen') {
+	    if (frm.doc.territory == 'Alle Regionen') {
 	        validation_error(frm, 'territory', __("Bitte wählen Sie eine Region aus."));
 	    }
-	    if (!['de','en'].includes(cur_frm.doc.language)) {
+		if (frm.doc.territory == 'Europa') {
+	        validation_error(frm, 'territory', __("Bitte statt 'Europa' die Region 'Europa übrige' auswählen."));
+	    }
+	    if (!['de','en'].includes(frm.doc.language)) {
 	        validation_error(frm, 'language', __("Bitte eine gültige Drucksprache wählen"));
 	    }
-		if(!cur_frm.doc.eori_number) {
-			if(! ['Schweiz','Liechtenstein'].includes(cur_frm.doc.territory)) {
-				frappe.show_alert({message: "Bitte vor dem Anlegen einer Rechnung für diesen Kunden die EORI-Nummer erfassen. Diese ist für Exporte in die EU zwingend erforderlich.", indicator: 'orange'}, 10);
-			}
+		if(!frm.doc.eori_number) {
+			frappe.db.get_value("Territory", frm.doc.territory, "parent_territory").then(r => {
+				if(r.message && r.message.parent_territory == 'Europa') {
+					frappe.show_alert({message: "Bitte vor dem Anlegen einer Rechnung für diesen Kunden die EORI-Nummer erfassen. Diese ist für Exporte in die EU zwingend erforderlich.", indicator: 'orange'}, 10);
+				}
+			});
 		}
-		else if(!cur_frm.doc.eori_number.match(/^[A-Z]{2}[0-9]{4,15}$/)) {
+		else if(!frm.doc.eori_number.match(/^[A-Z]{2}[0-9]{4,15}$/)) {
 			validation_error(frm, 'eori_number', __("Die EORI-Nummer muss aus einem ISO-Ländercode und einer Ziffernfolge bestehen."));
 		}
 	},
 	
 	territory(frm) {
 	    // Sprache ggf. überschreiben (irgendwie scheint "de" vorgegeben zu werden)
-        if (['Schweiz','Deutschland','Österreich','Liechtenstein'].includes(cur_frm.doc.territory)) {
-            cur_frm.set_value('language','de');
+        if (['Schweiz','Deutschland','Österreich','Liechtenstein'].includes(frm.doc.territory)) {
+            frm.set_value('language','de');
         }
         else {
-            cur_frm.set_value('language','en');
+            frm.set_value('language','en');
         }
-	    if (!cur_frm.doc.taxes_and_charges) {
-	        if (['Schweiz','Liechtenstein'].includes(cur_frm.doc.territory)) {
-	            cur_frm.set_value('taxes_and_charges',"7.7% MWST (302)");
+	    if (!frm.doc.taxes_and_charges) {
+	        if (['Schweiz','Liechtenstein'].includes(frm.doc.territory)) {
+	            frm.set_value('taxes_and_charges',"8.1% MWST (303)");
 	        }
 	        else {
-	            cur_frm.set_value('taxes_and_charges',"Export 0% MWST (220) - ST");
+	            frm.set_value('taxes_and_charges',"Export 0% MWST (220) - ST");
 	        }	        
 	    }
 	},
 	
 	refresh(frm) {
-	    cur_frm.set_df_property('primary_address_and_contact_detail','hidden','1');
+	    frm.set_df_property('primary_address_and_contact_detail','hidden','1');
 	},
 
 	onload_post_render(frm) {
