946eec8e1a4aff9c06ec92f45d12c7e325fd561f
Martin Zoller <martin.zoller@senstech.ch>
2024-03-11 11:23:28 +0100

    Sales Order: cur_frm => frm

--- senstech/fixtures/custom_scripts/Sales Order.js
+++ senstech/fixtures/custom_scripts/Sales Order.js
@@ -31,38 +31,38 @@ frappe.ui.form.on('Sales Order', {
         calculate_versanddatum(frm);
     },
     refresh(frm) {
-        if (cur_frm.doc.customer_address && cur_frm.doc.shipping_address_name) {
-            update_address_display(frm, ['address_display', 'shipping_address'], [cur_frm.doc.customer_address, cur_frm.doc.shipping_address_name], true);
+        if (frm.doc.customer_address && frm.doc.shipping_address_name) {
+            update_address_display(frm, ['address_display', 'shipping_address'], [frm.doc.customer_address, frm.doc.shipping_address_name], true);
         } else {
-            if (cur_frm.doc.customer_address) {
-                update_address_display(frm, 'address_display', cur_frm.doc.customer_address);
+            if (frm.doc.customer_address) {
+                update_address_display(frm, 'address_display', frm.doc.customer_address);
             }
-            if (cur_frm.doc.shipping_address_name) {
-                update_address_display(frm, 'shipping_address', cur_frm.doc.shipping_address_name);
+            if (frm.doc.shipping_address_name) {
+                update_address_display(frm, 'shipping_address', frm.doc.shipping_address_name);
             }
         }
         setTimeout(function(){
             // Lieferdatum kürzer darstellen
             reformat_delivery_dates();
         }, 1000);
-        if(cur_frm.doc.__islocal) {
+        if(frm.doc.__islocal) {
             // ggf. Kundendaten abrufen
-			if(cur_frm.doc.items){
-				if(cur_frm.doc.items[0].blanket_order) {
+			if(frm.doc.items){
+				if(frm.doc.items[0].blanket_order) {
 					setTimeout(function(){
-						fetch_templates_from_blanket_order(frm, cur_frm.doc.items[0].blanket_order);
+						fetch_templates_from_blanket_order(frm, frm.doc.items[0].blanket_order);
 					}, 1000);
 				}
 			}
-            if (!cur_frm.doc.taxes_and_charges || !cur_frm.doc.payment_terms_template){
+            if (!frm.doc.taxes_and_charges || !frm.doc.payment_terms_template){
                 setTimeout(function(){
                     fetch_templates_from_customer(frm);
                 }, 1000);
             }
             // ggf. Position der 1. Zeile korrekt setzen (Bugfix, offenbar wird diese Zeile neu automatisch erzeugt?)
-            if(cur_frm.doc.items) {
-                if(cur_frm.doc.items[0].position == 0) {
-                    frappe.model.set_value(cur_frm.doc.items[0].doctype,cur_frm.doc.items[0].name,'position',10);
+            if(frm.doc.items) {
+                if(frm.doc.items[0].position == 0) {
+                    frappe.model.set_value(frm.doc.items[0].doctype,frm.doc.items[0].name,'position',10);
                 }
             }
         }
@@ -77,11 +77,11 @@ frappe.ui.form.on('Sales Order', {
         jQuery('div[data-fieldname="base_in_words"]').hide();
     },
     before_submit(frm) {
-        cur_frm.doc.submitted_by = frappe.user.name;
+        frm.doc.submitted_by = frappe.user.name;
     },
     on_submit(frm) {
 		// Chargen werden serverseitig angelegt und hier nur abgefragt
-		frappe.db.get_list("Batch", { fields: ['batch_id'], filters: { batch_id: ['LIKE', cur_frm.docname+"-P%A"] }}).then(res => {
+		frappe.db.get_list("Batch", { fields: ['batch_id'], filters: { batch_id: ['LIKE', frm.docname+"-P%A"] }}).then(res => {
 			res.forEach(row => {
 				frappe.show_alert({message: __("Produktionscharge für Entwicklungsauftrag wurde automatisch angelegt:")+' <a href="#Form/Batch/'+row.batch_id+'">'+row.batch_id+'</a>', indicator: 'green'}, 10);
 			});
@@ -116,11 +116,11 @@ function calculate_versanddatum(frm) {
     frappe.call({
         "method": "senstech.scripts.sales_order_tools.calculate_versanddatum",
         "args": {
-            "so": cur_frm.doc.name
+            "so": frm.doc.name
         },
         "callback": function(response) {
             if (response.message == 'updated') {
-                cur_frm.reload_doc();
+                frm.reload_doc();
             }
         }
     });
@@ -128,17 +128,17 @@ function calculate_versanddatum(frm) {
 
 
 function fetch_templates_from_customer(frm) {
-	if(!cur_frm.doc.customer) {
+	if(!frm.doc.customer) {
         return;
     }
 
-    frappe.db.get_doc("Customer", cur_frm.doc.customer).then(customer => {
+    frappe.db.get_doc("Customer", frm.doc.customer).then(customer => {
         if(customer) {
-    		if (!cur_frm.doc.taxes_and_charges && customer.taxes_and_charges) {
-    				cur_frm.set_value('taxes_and_charges', customer.taxes_and_charges);
+    		if (!frm.doc.taxes_and_charges && customer.taxes_and_charges) {
+    				frm.set_value('taxes_and_charges', customer.taxes_and_charges);
     		}
-    		if(!cur_frm.doc.payment_terms_template && customer.payment_terms){
-    				cur_frm.set_value('payment_terms_template', customer.payment_terms);
+    		if(!frm.doc.payment_terms_template && customer.payment_terms){
+    				frm.set_value('payment_terms_template', customer.payment_terms);
     		}
         }
     });
@@ -151,8 +151,8 @@ function fetch_templates_from_blanket_order(frm, blanket_order) {
     frappe.db.get_doc("Blanket Order", blanket_order).then(bo => {
         if(bo) {
     		if(bo.payment_terms) {
-					cur_frm.set_value('payment_terms_template', '');
-    				cur_frm.set_value('payment_terms_template', bo.payment_terms);
+					frm.set_value('payment_terms_template', '');
+    				frm.set_value('payment_terms_template', bo.payment_terms);
 					frappe.show_alert({message: __("Zahlungsbedingungen aus Rahmenauftrag übernommen:")+' '+bo.payment_terms, indicator: 'green'}, 10);
     		}
         }
